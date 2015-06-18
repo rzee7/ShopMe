02c47ec65ddba414c2ce8350c83491f37e11ade1
GagnePadam <gagandeep@weexcelindia.com.com>
2015-06-18 20:29:14 +0530

    Models folder

---
+++ ShopMe.Core/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ShopMe.Core
+{
+	public class Category
+	{
+		public Category ()
+		{
+			
+		}
+
+		public Int32 CategoryId { get; set; }
+		public string CategoryName { get; set;}
+		public bool IsActive { get; set;}
+		public DateTime CreatedOnUtc { get; set;}
+		public Nullable<DateTime> UpdatedOnUtc { get; set; }
+	}
+}
+
