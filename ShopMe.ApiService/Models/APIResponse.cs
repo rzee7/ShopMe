98aa2587abdd95ba4d5bd04965ce3bd189adf6dd
gagnepadam <gagandeep@weexcelindia.com>
2015-06-25 20:21:41 +0530

    committing APiResponse

---
+++ ShopMe.ApiService/Models/APIResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopMe.ApiService.Models
+{
+    public class APIResponse
+    {
+        public bool IsSucess { get; set; }
+        public string Message { get; set; }
+        public object ResponseData { get; set; }
+    }
+}
